82a2b61a469940043faefab4be0e7edde35d5221
DavidLBrandt <david.l.brandt@comcast.net>
2019-04-05 07:47:58 -0700

    Initial commit

---
+++ Homework 1/HelloWorld/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HelloWorld
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //WindowState = WindowState.Maximized;
+        }
+
+        private void UxSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Submitting password:" + uxPassword.Text);
+        }
+
+        private void UxName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            ValidateSubmit();
+        }
+
+        private void UxPassword_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            ValidateSubmit();
+        }
+
+        private void ValidateSubmit()
+        {
+            uxSubmit.IsEnabled = (uxName.Text != "" && uxPassword.Text != "");
+        }
+    }
+}
