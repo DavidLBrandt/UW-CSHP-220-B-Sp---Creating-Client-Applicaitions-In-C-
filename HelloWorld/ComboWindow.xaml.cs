528b91a9e26c172c55f006fd0b4ec619a41eb964
DavidLBrandt <david.l.brandt@comcast.net>
2019-04-19 14:37:36 -0700

    Homework 3: Sorting ListView Columns

---
+++ HelloWorld/ComboWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data.Entity;
+
+namespace HelloWorld
+{
+    /// <summary>
+    /// Interaction logic for CombowWindow.xaml
+    /// </summary>
+    public partial class CombowWindow : Window
+    {
+        public CombowWindow()
+        {
+            InitializeComponent();
+
+            var sample = new SampleEntities();
+            sample.Users.Load();
+            uxComboBox.ItemsSource = sample.Users.Local;
+        }
+
+        private void UxListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            uxGrid.DataContext = e.AddedItems[0];
+        }
+    }
+
+
+}
---
+++ HelloWorld/SecondWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HelloWorld
+{
+    /// <summary>
+    /// Interaction logic for SecondWindow.xaml
+    /// </summary>
+    public partial class SecondWindow : Window
+    {
+        public SecondWindow()
+        {
+            InitializeComponent();
+
+            var users = new List<Models.User>();
+
+            users.Add(new Models.User { Name = "Dave", Password = "1DavePwd" });
+            users.Add(new Models.User { Name = "Steve", Password = "2StevePwd" });
+            users.Add(new Models.User { Name = "Lisa", Password = "3LisaPwd" });
+
+            uxList.ItemsSource = users;
+
+            CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(uxList.ItemsSource);
+            view.SortDescriptions.Add(new SortDescription("Name", ListSortDirection.Ascending));
+
+            AddHandler(GridViewColumnHeader.ClickEvent, new RoutedEventHandler(ListView_OnColumnClick));
+        }
+
+        private void ListView_OnColumnClick(object sender, RoutedEventArgs e)
+        {
+            CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(uxList.ItemsSource);
+            string clickedColumn = ((System.Windows.Controls.ContentControl)e.OriginalSource).Content.ToString();
+            ListSortDirection sortDirection = ListSortDirection.Ascending;
+
+            if (view.SortDescriptions.Count != 0)
+            {
+                if (clickedColumn == view.SortDescriptions.First<SortDescription>().PropertyName)
+                {
+                    sortDirection = view.SortDescriptions.First<SortDescription>().Direction == ListSortDirection.Ascending ? ListSortDirection.Descending : ListSortDirection.Ascending;
+                };
+            }
+
+            view.SortDescriptions.Clear();
+            view.SortDescriptions.Add(new SortDescription(clickedColumn, sortDirection));
+        }
+    }
+}
