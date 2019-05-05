cff15b3424e23d4f67bd4f78a462d8ed96e7f50b
DavidLBrandt <david.l.brandt@comcast.net>
2019-05-04 17:01:32 -0700

    Homework 4: ZipCode TextBox

---
+++ HelloWorld/NumericControl.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HelloWorld
+{
+    /// <summary>
+    /// Interaction logic for IntControl.xaml
+    /// </summary>
+    public partial class NumericControl : UserControl
+    {
+        public NumericControl()
+        {
+            InitializeComponent();
+        }
+
+        private void UxIntTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !IsTextAllowed(e.Text);
+        }
+
+        private static readonly Regex _regex = new Regex("[^0-9.-]+"); //regex that matches disallowed text
+        private static bool IsTextAllowed(string text)
+        {
+            return !_regex.IsMatch(text);
+        }
+
+        private void UxIntTextBox_Pasting(object sender, DataObjectPastingEventArgs e)
+        {
+            if (e.DataObject.GetDataPresent(typeof(String)))
+            {
+                String text = (String)e.DataObject.GetData(typeof(String));
+                if (!IsTextAllowed(text))
+                {
+                    e.CancelCommand();
+                }
+            }
+            else
+            {
+                e.CancelCommand();
+            }
+        }
+    }
+}
---
+++ Homework 4/CSHP22B_HW4_ZipCodeTextBox/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CSHP22B_HW4_ZipCodeTextBox
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private static bool IsTextAllowed(string text)
+        {
+            Regex _usZipRegEx = new Regex( @"^\d{5}(?:[-\s]\d{4})?$" );
+            Regex _caZipRegEx = new Regex( @"^([ABCEGHJKLMNPRSTVXY]\d[ABCEGHJKLMNPRSTVWXYZ])\ {0,1}(\d[ABCEGHJKLMNPRSTVWXYZ]\d)$" );
+
+            return _usZipRegEx.IsMatch(text) || _caZipRegEx.IsMatch(text);
+        }
+
+        private void UiSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Submitted!");
+        }
+
+        private void UiZipCode_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (IsTextAllowed(uiZipCode.Text))
+            {
+                uiSubmit.IsEnabled = true;
+            }
+            else
+            {
+                uiSubmit.IsEnabled = false;
+            }
+        }
+    }
+}
